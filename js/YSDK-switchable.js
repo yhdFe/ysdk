974f55178db73a9c813b616240c87057584c9377
luckyrobin <icestear@gmail.com>
2014-11-19 17:35:44 +0800

    ysdk

--- js/YSDK-switchable.js
+++ js/YSDK-switchable.js
@@ -41,7 +41,7 @@ YSDK.add(function () {
                 config.markupType = 2;
             }
         }
-        config = $.merge(Switchable.Config, config);
+        config = YSDK.merge(Switchable.Config, config);
 
         /**
          * the container of widget
@@ -207,13 +207,13 @@ YSDK.add(function () {
             var self = this, cfg = self.config,
                 ul = $('<ul>'), li, i;
 
-            ul.className = cfg.navCls;
+            ul.addClass(cfg.navCls);
             for (i = 0; i < len; i++) {
                 li = $('<li>');
                 if (i === self.activeIndex) {
-                    li.className = cfg.activeTriggerCls;
+                    li.addClass(cfg.activeTriggerCls);
                 }
-                li.innerHTML = i + 1;
+                li.html(i + 1);
                 ul.append(li);
             }
 
@@ -309,7 +309,7 @@ YSDK.add(function () {
                 fromIndex = activeIndex * steps, toIndex = index * steps;
 
             if (!self._triggerIsValid(index)) return self; // 再次避免重复触发
-            if (self.trigger(EVENT_BEFORE_SWITCH, {toIndex: index}) === false) return self;
+            if ($(self).trigger(EVENT_BEFORE_SWITCH, {toIndex: index}) === false) return self;
 
             // switch active trigger
             if (cfg.hasTriggers) {
@@ -486,7 +486,7 @@ YSDK.add(function () {
      */
     YSDK.mix(Switchable.Config, {
         effect: NONE, // 'scrollx', 'scrolly', 'fade' 或者直接传入 custom effect fn
-        duration: .5, // 动画的时长
+        duration: 500, // 动画的时长
         easing: 'easeNone', // easing method
         nativeAnim: true
     });
@@ -624,7 +624,7 @@ YSDK.add(function () {
     /**
      * 覆盖切换方法
      */
-    Switchable.prototype.switchView = function (fromEls, toEls, index, direction) {
+    Switchable.prototype._switchView = function (fromEls, toEls, index, direction) {
         var self = this, cfg = self.config,
             effect = cfg.effect,
             fn = $.isFunction(effect) ? effect : Effects[effect];
@@ -682,7 +682,7 @@ YSDK.add(function () {
 
         // 开始动画
         if (self.anim) $(this).stop();
-        self.anim = $(self.content)(props, {duration: cfg.duration, easing: cfg.easing}, function () {
+        self.anim = $(self.content).animate(props, {duration: cfg.duration, easing: cfg.easing}, function () {
             if (isCritical) {
                 // 复原位置
                 resetPosition.call(self, self.panels, index, isBackward, prop, viewDiff);
@@ -801,7 +801,7 @@ YSDK.add(function () {
 
             if (!DataLazyload || !type || !flag) return; // 没有延迟项
 
-            host.on(EVENT_BEFORE_SWITCH, loadLazyData);
+            $(host).bind(EVENT_BEFORE_SWITCH, loadLazyData);
 
             /**
              * 加载延迟数据
@@ -813,7 +813,7 @@ YSDK.add(function () {
 
                 DataLazyload.loadCustomLazyData(host.panels.slice(from, to), type);
                 if (isAllDone()) {
-                    host.detach(EVENT_BEFORE_SWITCH, loadLazyData);
+                    $(host).unbind(EVENT_BEFORE_SWITCH, loadLazyData);
                 }
             }
 
@@ -891,208 +891,198 @@ YSDK.add(function () {
     YSDK.Tabs = Tabs;
 
 });
-///**
-// * Tabs Widget
-// * @creator     玉伯<lifesinger@gmail.com>
-// */
-//KISSY.add('slide', function (S) {
-//
-//    /**
-//     * 默认配置，和 Switchable 相同的部分此处未列出
-//     */
-//    var defaultConfig = {
-//        autoplay: true,
-//        circular: true
-//    };
-//
-//    /**
-//     * Slide Class
-//     * @constructor
-//     */
-//    function Slide(container, config) {
-//        var self = this;
-//
-//        // factory or constructor
-//        if (!(self instanceof Slide)) {
-//            return new Slide(container, config);
-//        }
-//
-//        Slide.superclass.constructor.call(self, container, S.merge(defaultConfig, config));
-//    }
-//
-//    S.extend(Slide, S.Switchable);
-//    S.Slide = Slide;
-//
-//}, {host: 'switchable'});
-///**
-// * Carousel Widget
-// * @creator  玉伯<lifesinger@gmail.com>
-// */
-//KISSY.add('carousel', function (S, undefined) {
-//
-//    var DOM = S.DOM, Event = S.Event,
-//        CLS_PREFIX = 'ks-switchable-', DOT = '.',
-//        PREV_BTN = 'prevBtn', NEXT_BTN = 'nextBtn',
-//
-//        /**
-//         * 默认配置，和 Switchable 相同的部分此处未列出
-//         */
-//        defaultConfig = {
-//            circular: true,
-//            prevBtnCls: CLS_PREFIX + 'prev-btn',
-//            nextBtnCls: CLS_PREFIX + 'next-btn',
-//            disableBtnCls: CLS_PREFIX + 'disable-btn'
-//        };
-//
-//    /**
-//     * Carousel Class
-//     * @constructor
-//     */
-//    function Carousel(container, config) {
-//        var self = this;
-//
-//        // factory or constructor
-//        if (!(self instanceof Carousel)) {
-//            return new Carousel(container, config);
-//        }
-//
-//        // 插入 carousel 的初始化逻辑
-//        self.on('init', function () {
-//            init_carousel(self);
-//        });
-//
-//        // call super
-//        Carousel.superclass.constructor.call(self, container, S.merge(defaultConfig, config));
-//    }
-//
-//    S.extend(Carousel, S.Switchable);
-//    S.Carousel = Carousel;
-//
-//    /**
-//     * Carousel 的初始化逻辑
-//     * 增加了:
-//     *   self.prevBtn
-//     *   self.nextBtn
-//     */
-//    function init_carousel(self) {
-//        var cfg = self.config, disableCls = cfg.disableBtnCls;
-//
-//        // 获取 prev/next 按钮，并添加事件
-//        S.each(['prev', 'next'], function (d) {
-//            var btn = self[d + 'Btn'] = S.get(DOT + cfg[d + 'BtnCls'], self.container);
-//
-//            Event.on(btn, 'click', function (ev) {
-//                ev.preventDefault();
-//                if (!DOM.hasClass(btn, disableCls)) self[d]();
-//            });
-//        });
-//
-//        // 注册 switch 事件，处理 prevBtn/nextBtn 的 disable 状态
-//        // circular = true 时，无需处理
-//        if (!cfg.circular) {
-//            self.on('switch', function (ev) {
-//                var i = ev.currentIndex,
-//                    disableBtn = (i === 0) ? self[PREV_BTN]
-//                        : (i === self.length - 1) ? self[NEXT_BTN]
-//                        : undefined;
-//
-//                DOM.removeClass([self[PREV_BTN], self[NEXT_BTN]], disableCls);
-//                if (disableBtn) DOM.addClass(disableBtn, disableCls);
-//            });
-//        }
-//
-//        // 触发 itemSelected 事件
-//        Event.on(self.panels, 'click focus', function () {
-//            self.fire('itemSelected', {item: this});
-//        });
-//    }
-//
-//}, {host: 'switchable'});
-//
-//
-///**
-// * NOTES:
-// *
-// * 2010.07
-// *  - 添加对 prevBtn/nextBtn 的支持
-// *  - 添加 itemSelected 事件
-// *
-// * TODO:
-// *  - 对键盘事件的支持，比如 Up/Down 触发 prevItem/nextItem, PgDn/PgUp 触发 prev/next
-// *  - itemSelected 时，自动居中的特性
-// */
-///**
-// * Accordion Widget
-// * @creator  沉鱼<fool2fish@gmail.com>
-// */
-//KISSY.add('accordion', function (S) {
-//
-//    var DOM = S.DOM,
-//        DISPLAY = 'display', BLOCK = 'block', NONE = 'none',
-//
-//        defaultConfig = {
-//            markupType: 1,
-//            triggerType: 'click',
-//            multiple: false
-//        };
-//
-//    /**
-//     * Accordion Class
-//     * @constructor
-//     */
-//    function Accordion(container, config) {
-//        var self = this;
-//
-//        // factory or constructor
-//        if (!(self instanceof Accordion)) {
-//            return new Accordion(container, config);
-//        }
-//
-//        Accordion.superclass.constructor.call(self, container, S.merge(defaultConfig, config));
-//
-//        // multiple 模式时，switchTrigger 在 switchView 时已经实现
-//        if (self.config.multiple) {
-//            self._switchTrigger = function () {
-//            }
-//        }
-//    }
-//
-//    S.extend(Accordion, S.Switchable);
-//    S.Accordion = Accordion;
-//
-//    S.augment(Accordion, {
-//
-//        /**
-//         * 重复触发时的有效判断
-//         */
-//        _triggerIsValid: function (index) {
-//            // multiple 模式下，再次触发意味着切换展开/收缩状态
-//            return this.activeIndex !== index || this.config.multiple;
-//        },
-//
-//        /**
-//         * 切换视图
-//         */
-//        _switchView: function (fromPanels, toPanels, index) {
-//            var self = this, cfg = self.config,
-//                panel = toPanels[0];
-//
-//            if (cfg.multiple) {
-//                DOM.toggleClass(self.triggers[index], cfg.activeTriggerCls);
-//                DOM.css(panel, DISPLAY, panel.style[DISPLAY] == NONE ? BLOCK : NONE);
-//                this._fireOnSwitch(index);
-//            }
-//            else {
-//                Accordion.superclass._switchView.call(self, fromPanels, toPanels, index);
-//            }
-//        }
-//    });
-//
-//}, {host: 'switchable'});
-//
-///**
-// * TODO:
-// *
-// *  - 支持动画
-// *
-// */
+/**
+* Tabs Widget
+* @creator     玉伯<lifesinger@gmail.com>
+*/
+YSDK.add(function () {
+
+    /**
+     * 默认配置，和 Switchable 相同的部分此处未列出
+     */
+    var defaultConfig = {
+        autoplay: true,
+        circular: true
+    };
+
+    /**
+     * Slide Class
+     * @constructor
+     */
+    function Slide(container, config) {
+        var self = this;
+
+        // factory or constructor
+        if (!(self instanceof Slide)) {
+            return new Slide(container, config);
+        }
+
+        Slide.superclass.constructor.call(self, container, YSDK.merge(defaultConfig, config));
+    }
+
+    YSDK.extend(Slide, YSDK.Switchable);
+    YSDK.Slide = Slide;
+
+});
+/**
+* Carousel Widget
+* @creator  玉伯<lifesinger@gmail.com>
+*/
+YSDK.add(function () {
+
+    var CLS_PREFIX = 'ks-switchable-', DOT = '.',
+        PREV_BTN = 'prevBtn', NEXT_BTN = 'nextBtn',
+
+        /**
+         * 默认配置，和 Switchable 相同的部分此处未列出
+         */
+        defaultConfig = {
+            circular: true,
+            prevBtnCls: CLS_PREFIX + 'prev-btn',
+            nextBtnCls: CLS_PREFIX + 'next-btn',
+            disableBtnCls: CLS_PREFIX + 'disable-btn'
+        };
+
+    /**
+     * Carousel Class
+     * @constructor
+     */
+    function Carousel(container, config) {
+        var self = this;
+
+        // factory or constructor
+        if (!(self instanceof Carousel)) {
+            return new Carousel(container, config);
+        }
+
+        // 插入 carousel 的初始化逻辑
+        $(self).bind('init', function () {
+            init_carousel(self);
+        });
+
+        // call super
+        Carousel.superclass.constructor.call(self, container, YSDK.merge(defaultConfig, config));
+    }
+
+    YSDK.extend(Carousel, YSDK.Switchable);
+    YSDK.Carousel = Carousel;
+
+    /**
+     * Carousel 的初始化逻辑
+     * 增加了:
+     *   self.prevBtn
+     *   self.nextBtn
+     */
+    function init_carousel(self) {
+        var cfg = self.config, disableCls = cfg.disableBtnCls;
+
+        // 获取 prev/next 按钮，并添加事件
+        $.each(['prev', 'next'], function (i,d) {
+            var btn = self[d + 'Btn'] = $(DOT + cfg[d + 'BtnCls'], self.container);
+
+            $(btn).bind('click', function (ev) {
+                ev.preventDefault();
+                if (!$(btn).hasClass(disableCls)) self[d]();
+            });
+        });
+
+        // 注册 switch 事件，处理 prevBtn/nextBtn 的 disable 状态
+        // circular = true 时，无需处理
+        if (!cfg.circular) {
+            $(this).bind('switch', function (ev) {
+                var i = ev.currentIndex,
+                    disableBtn = (i === 0) ? self[PREV_BTN]
+                        : (i === self.length - 1) ? self[NEXT_BTN]
+                        : undefined;
+
+                $(self[PREV_BTN], self[NEXT_BTN]).removeClass(disableCls);
+                if (disableBtn) $(disableBtn).addClass(disableCls);
+            });
+        }
+
+        // 触发 itemSelected 事件
+        $(self.panels).bind('click focus', function () {
+            $(self).trigger('itemSelected', {item: this});
+        });
+    }
+
+});
+
+
+/**
+* NOTES:
+*
+* 2010.07
+*  - 添加对 prevBtn/nextBtn 的支持
+*  - 添加 itemSelected 事件
+*
+* TODO:
+*  - 对键盘事件的支持，比如 Up/Down 触发 prevItem/nextItem, PgDn/PgUp 触发 prev/next
+*  - itemSelected 时，自动居中的特性
+*/
+/**
+* Accordion Widget
+* @creator  沉鱼<fool2fish@gmail.com>
+*/
+YSDK.add(function () {
+
+    var DISPLAY = 'display', BLOCK = 'block', NONE = 'none',
+
+        defaultConfig = {
+            markupType: 1,
+            triggerType: 'click',
+            multiple: false
+        };
+
+    /**
+     * Accordion Class
+     * @constructor
+     */
+    function Accordion(container, config) {
+        var self = this;
+
+        // factory or constructor
+        if (!(self instanceof Accordion)) {
+            return new Accordion(container, config);
+        }
+
+        Accordion.superclass.constructor.call(self, container, YSDK.merge(defaultConfig, config));
+
+        // multiple 模式时，switchTrigger 在 switchView 时已经实现
+        if (self.config.multiple) {
+            self._switchTrigger = function () {
+            }
+        }
+    }
+
+    YSDK.extend(Accordion, YSDK.Switchable);
+    YSDK.Accordion = Accordion;
+
+    Accordion.prototype._triggerIsValid = function (index) {
+        // multiple 模式下，再次触发意味着切换展开/收缩状态
+        return this.activeIndex !== index || this.config.multiple;
+    }
+
+    Accordion.prototype._switchView= function (fromPanels, toPanels, index) {
+        var self = this, cfg = self.config,
+            panel = toPanels[0];
+
+        if (cfg.multiple) {
+            $(self.triggers[index]).toggleClass( cfg.activeTriggerCls);
+            $(panel).css( DISPLAY, panel.style[DISPLAY] == NONE ? BLOCK : NONE);
+            this._fireOnSwitch(index);
+        }
+        else {
+            Accordion.superclass._switchView.call(self, fromPanels, toPanels, index);
+        }
+    }
+
+
+});
+
+/**
+* TODO:
+*
+*  - 支持动画
+*
+*/
